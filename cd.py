af843a0ae5d4fa4ab1491209ef4a6a440479e15b
Marcos Vinicius <marcosvn1152@gmail.com>
2026-02-15 18:16:15 -0300

    consertando o while True

--- cd.py
+++ cd.py
@@ -70,11 +70,10 @@ def deletar_usuario(email): #funçao delet
 
 
 
-
 while True:
 
+    escolha = input('1-adicionar|2-login|3-atualizar |4-deletar | sair\n')
 
-    escolha=input('1-adicionar|2-login|3-atualizar |4-deletar | sair\n')
     if escolha == '1':
         email = input('Email: ')
         nome = input('Nome: ')
@@ -83,43 +82,40 @@ while True:
 
         print(adicionar_usuario(email, nome, cpf, senha))
 
-    
-    elif escolha =='2':
-        email=input('digite seu email')
-        
-        senha=input('digite sua senha:')
-        print(autenticar_usuario(email,senha))
-   
+    elif escolha == '2':
+        email = input('digite seu email: ')
+        senha = input('digite sua senha: ')
+        print(autenticar_usuario(email, senha))
 
     elif escolha == "3":
         email = input("Digite o email do usuário: ")
 
-    novo_nome = input("Novo nome (Enter para manter): ")
-    novo_cpf = input("Novo CPF (Enter para manter): ")
-    nova_senha = input("Nova senha (Enter para manter): ")
+        novo_nome = input("Novo nome (Enter para manter): ")
+        novo_cpf = input("Novo CPF (Enter para manter): ")
+        nova_senha = input("Nova senha (Enter para manter): ")
 
-    dados = {}
+        dados = {}
 
-    if novo_nome:
-        dados["nome"] = novo_nome
-    if novo_cpf:
-        dados["cpf"] = novo_cpf
-    if nova_senha:
-        dados["senha"] = nova_senha
+        if novo_nome:
+            dados["nome"] = novo_nome
+        if novo_cpf:
+            dados["cpf"] = novo_cpf
+        if nova_senha:
+            dados["senha"] = nova_senha
 
-    print(atualizar_usuario(email, **dados))
+        print(atualizar_usuario(email, **dados))
+        print(Cadastro_usarios)
 
-        
-    if escolha=='4':
-        email=input('digite o email que deseja deletar:')
+    elif escolha == '4':
+        email = input('digite o email que deseja deletar: ')
         print(deletar_usuario(email))
 
-    elif escolha=='sair':
+    elif escolha == 'sair':
         print('fim do programa')
         break
-    
+
     else:
-        print('opçao invalida')
+        print('opção inválida')
 
 
                               
