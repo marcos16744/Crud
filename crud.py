d4ca2e075658101ef6a68c3806c294de1f0ffa1b
Marcos Vinicius <marcosvn1152@gmail.com>
2026-02-17 17:28:05 -0300

    arquivo crud.py separado pelas funçoes

--- crud.py
+++ crud.py
@@ -19,7 +19,8 @@ usuarios[2]={
 }
 
 
-def adicionar_usuario(id, nome, email, senha, cpf,ativo):
+
+def adicionar_usuario(id, nome, email, senha, cpf,ativo): #funçao creat
     if id  in usuarios:
         return 'ja esta cadastrado'
     
@@ -36,34 +37,57 @@ def adicionar_usuario(id, nome, email, senha, cpf,ativo):
 
 
 
-
-
-#para confirmar se esta funcionado a funçao usa um while True perguntando cada variavel e depois da o print da funçao e os parametros
-# ou para confirmar da o print da funçao e ao inves de colocar os parametros , coloca o resultado que voce ja quiser usa string para nome , se ano usar ele interpreta como variavel
-#mas voce precisa colocar  um return  na funçao para ver se deu certo se colocar um return que 'vai dar errado' ( fazendo parte da estrutura da funçao) , se 'estiver certo ' ele nao vai aparecer ou entao dar o print do dicinario para ver se ele foi adicionado
-
-
-    
-print(adicionar_usuario(
-    3,
-    "Bruna marquezine",
-    "bruna@gmail.com",
-    "1234",
-    "999999999",
-    True
-))
-
+print(usuarios)
+print(adicionar_usuario(4,'jose','jose@gmail.com','1144','4354343','ativo'))
 print(usuarios)
 
-# while True:
-#     id=input('qual o id:')
-#     nome=input('qual o nome:')
-#     email=input('qual o email:')
-#     senha=input('qual a senha:')
-#     cpf=input('qual o cpf:')
-#     ativo=input('qual o estado:')
 
-#     print(adicionar_usuario(id, nome, email, senha, cpf,ativo))
-#     print(usuarios)
 
+
+def buscar_usuario(id): #funçao read
+    if id not in usuarios:
+        return "Usuário não encontrado"
+    
+    return usuarios[id]
+
+
+
+print(usuarios)
+print(buscar_usuario(2))
+
+
+
+
+def atualizar_senha(id, nova_senha): #funçao update
+    if id not in  usuarios:
+        return 'Erro'
+    usuarios[id]['senha']= nova_senha
+    return 'senha atualizada com sucesso'
+
+print(usuarios)
+print(atualizar_senha(4,'mangaratiba'))
+print(usuarios)
+
+
+def atualizar_usuario(id, campo, novo_valor): #funçao update mais geral
+    if id not in usuarios:
+        return 'Usuário não encontrado'
+    
+    usuarios[id][campo] = novo_valor
+    return 'Usuário atualizado com sucesso'
+
+
+
+
+
+def deletar_usuario(id): #funçao delet
+    if id in usuarios:
+        del usuarios[id]
+        return ' usuarios deletado'
+    return 'usuario nao encontrado'
+
+
+print(usuarios)
+print(deletar_usuario(2))
+print(usuarios)
 
